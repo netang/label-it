ac72922ecd836a4e0d4961d8e3addefeb470c94d
netang <netang@yandex.ru>
2019-10-05 18:22:37 +0300

    Add 'Tasks' entity and migration scripts

--- server/models.py
+++ server/models.py
@@ -2,3 +2,9 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Integer, Numeric, Float, ForeignKey, String, Column, Date, Boolean, DateTime, BigInteger
 
 db = SQLAlchemy()
+
+class Tasks(db.Model):
+    """Tasks"""
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, primary_key = True)
+    name = Column(String)
