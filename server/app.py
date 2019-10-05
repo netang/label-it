d9e5ab893cd65fd873d74c887a77c2cac8d8cb92
netang <netang@yandex.ru>
2019-10-05 18:08:41 +0300

    server init

---
+++ server/app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_hashing import Hashing
+from models import db
+
+
+app = Flask(__name__)
+
+hashing = Hashing(app)
+
+# DB connection configuration
+POSTGRES = {
+    'user': 'postgres',
+    'pw': 'rtr1',
+    'db': 'label_it',
+    'host': 'localhost',
+    'port': '5432'
+}
+db_uri = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+""" Flask application to enable Cross Origin Resource Sharing (CORS).
+Anytime a client makes a request for a resource that resides on another machine as defined by
+protocol, IP address / domain name, or port number then additional headers associated with CORS must be added. """
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+app.config['SECRET_KEY'] = 'dgdfg454567egfdgs'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSON_AS_ASCII'] = False
+db.init_app(app)
+
---
+++ server/models.py
@@ -0,0 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, Numeric, Float, ForeignKey, String, Column, Date, Boolean, DateTime, BigInteger
+
+db = SQLAlchemy()
