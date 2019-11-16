ebaf302e2ed0a6b21b0bb30a7b82dbfa4c7a9420
netang <netang@yandex.ru>
2019-11-16 20:19:06 +0300

    list of tasks for labeling

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,27 +1,55 @@
 <template>
   <div id="app">
     <div class="row">
-      <div class="col-md-4">
-        <table class="table table-striped table-hover alert">
+      <div class="col-md-7">
+        <table class="table table-striped table-hover">
           <thead>
             <tr>
               <th scope="col">Имя разметки</th>
               <th scope="col">Описание</th>
+              <th scope="col">Инициатор</th>
+              <th scope="col">Прогресс</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">Разметка топиков</th>
-              <td>...</td>
+            <tr v-for="task in tasks" :key="task.id">
+              <th>{{task.name}}</th>
+              <td><pre>{{task.description}}</pre></td>
+              <td>{{task.initiator}}</td>
+              <td>{{task.progress}}</td>
+              <td><button class="btn btn-primary btn-sm mb-1">Перейти</button></td>
             </tr>
           </tbody>
         </table>
-  </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
+export default {
+  name: "main",
+  data: function() {
+    return {
+      tasks: null
+    };
+  },
+  methods: {
+    get_tasks: function() {
+      axios
+        .get("http://127.0.0.1:5000/api/tasks/")
+        .then(response => (this.tasks = response.data.tasks))
+        .catch(error => (console.error(error)));
+    }
+  },
+  mounted() {
+    this.get_tasks();
+  }
+};
+
 </script>
 
 <style>
@@ -32,5 +60,7 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  margin-left: 60px;
+  margin-right: 60px;
 }
 </style>
--- server/models.py
+++ server/models.py
@@ -8,11 +8,15 @@ class Tasks(db.Model):
     __tablename__ = 'tasks'
     id = db.Column(db.Integer, primary_key = True)
     name = Column(String)
+    description = Column(String)
+    initiator_id = Column(Integer)
 
     @property
     def serialize(self):
         """Return object data in easily serializeable format"""
         return {
             'id'             : self.id,
-            'name'           : self.name
+            'name'           : self.name,
+            'description'    : self.description,
+            'initiator_id'   : self.initiator_id
         }
